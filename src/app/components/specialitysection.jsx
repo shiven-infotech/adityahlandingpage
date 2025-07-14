fa2075237993e978bbdb64b256b426ce25e601be
Hardik Chouhan <chouhanhardik571@gmail.com>
2025-07-14 12:54:36 +0530

    changes

--- src/app/components/specialitysection.jsx
+++ src/app/components/specialitysection.jsx
@@ -1,10 +1,10 @@
 export default function SpecialitySection() {
   return (
     <section className="bg-[#FFFDD0] py-8  px-4  ">
-      <h2 className="text-3xl py-3 mt-4  font-bold text-green-700 text-center ">
+      <h2 className="text-3xl py-3 mt-4 mb-3  font-bold text-green-700 text-center ">
         Speciality of Dr. Shital's Line of Treatment
       </h2>
-      <p className="text-gray-700 text-lg leading-relaxed text-justify">
+      <p className="text-gray-700 text-lg  max-w-6xl mx-auto ">
         Dr. Shital's treatment approach is rooted in classical homoeopathy, emphasizing individualized care and long-term healing. Every patient is thoroughly evaluated, not just for physical symptoms but for emotional and lifestyle patterns as well. The specialty lies in targeting the root cause of the disease using minimal doses, ensuring safe, gentle, and effective results.
         <br /><br />
         Her line of treatment is especially beneficial for chronic ailments, hormonal imbalances, autoimmune conditions, children’s health issues, and psychological concerns. Dr. Shital's compassionate listening and deep case analysis stand out as hallmarks of her practice, offering hope where conventional medicine may fall short.
